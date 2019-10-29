bf55e664d9d150fd7b3c9a8c80086e260124ecbb
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-10-29 14:47:38 -0700

    Set up automatic slideshow

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,1 +1,16 @@
-console.log('Initial rendering')
+console.log('Initial rendering');
+
+
+var slideIndex = 0;
+let slideShow = () => {
+  let slides = document.querySelectorAll('.mySlides');
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(slideShow, 3000)
+};
+slideShow()
+
