af2dc970f519b407f508c856f670c02c823f3018
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-10-30 22:09:00 -0700

    Set up responsive nav bar

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,16 +1,21 @@
 console.log('Initial rendering');
 
-
 var slideIndex = 0;
-let slideShow = () => {
+const slideShow = () => {
   let slides = document.querySelectorAll('.mySlides');
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
-  slideIndex++
+  slideIndex++;
   if (slideIndex > slides.length) slideIndex = 1;
   slides[slideIndex - 1].style.display = 'block';
-  setTimeout(slideShow, 5000)
+  setTimeout(slideShow, 5000);
 };
-slideShow()
+slideShow();
 
+let mainNav = document.querySelector('#js-menu');
+let navBarToggle = document.querySelector('#js-navbar-toggle');
+
+navBarToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('active');
+});
